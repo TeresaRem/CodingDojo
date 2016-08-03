7c8e1e67a4fb1e0d62e430741bebe743f3e9ef25
Alex W <wapniarski@gmail.com>
2016-08-02 17:03:34 -0700

    added python folder

---
+++ Python/multiples.py
@@ -0,0 +1,11 @@
+# Create a program that prints all the odd numbers from 1 to 1000. 
+# Use the for loop and don't use array to do this exercise.
+for x in range(1,1001,2):
+	print x
+
+# Create another program that prints all the multiples of 5 from 5 to 1,000,000. 
+
+for x in range(5,1000000,5):
+	print x
+
+	
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 
 This is my coursework for Coding Dojo. The assignments are organized by date.
+
+
+## To Do
+
+-add git submodules for pacman and contact_card
+-https://git-scm.com/book/en/v2/Git-Tools-Submodules
