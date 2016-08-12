a0a75b436d380a7e690f3ce249690e199e13ed79
Alex W <wapniarski@gmail.com>
2016-08-11 19:16:53 -0700

    completed ninja_gold assignment

--- Flask/ninja_gold/server.py
+++ Flask/ninja_gold/server.py
@@ -1,25 +1,46 @@
 from flask import Flask, render_template, request, redirect, session
-import random
+import random, datetime
 
 app = Flask(__name__)
 app.secret_key = 'very secret'
 
 def farm():
-  session['gold'] += random.randrange(10,21)
+  g = random.randrange(10,21)
+  session['gold'] += g
+  session['messages'].insert(0,'Earned {} gold from the farm! ({})'.format(g,datetime.datetime.now()))
 
 def cave():
-  session['gold'] += random.randrange(5,11)
+  g = random.randrange(5,11)
+  session['gold'] += g
+  session['messages'].insert(0,'Earned {} gold from the cave! ({})'.format(g,datetime.datetime.now()))
 
 def house():
-  session['gold'] += random.randrange(2,6)
+  g = random.randrange(2,6)
+  session['gold'] += g
+  session['messages'].insert(0,'Earned {} gold from the house! ({})'.format(g,datetime.datetime.now()))
 
 def casino():
-  session['gold'] += random.randrange(-50,51)
+  g = random.randrange(-50,51)
+  session['gold'] += g
+  if g < 0:
+    session['messages'].insert(0,'Entered a casino and lost {} gold... Ouch! ({})'.format(abs(g),datetime.datetime.now()))
+    session['win_lose_color'] = 'red'    
+  else:
+    session['messages'].insert(0,'Entered a casino and won {} gold!!! ({})'.format(g,datetime.datetime.now()))
+    session['win_lose_color'] = 'green'    
+
+def time_now():
+  d = datetime.datetime.now()
+  # format = '%Y-%m-%d %H:%M %p'
+  # d = datetime.datetime.strptime(d, format)
+  return d
 
 @app.route('/')
 def index():
-  if session['gold'] != ''
+  if 'gold' not in session:
     session['gold'] = 0
+  if 'messages' not in session:
+    session['messages'] = []
 
   return render_template('index.html')
 
@@ -37,8 +58,7 @@ def process_money():
     house()
   else:
     casino()
-
-  print session['gold']
+    
   return redirect('/')
 
 @app.route('/reset', methods = ['POST'])
