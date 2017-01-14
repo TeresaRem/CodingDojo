689037f6ddc3c65afb5710aed4251ddc486fc6ae
Alex W <wapniarski@gmail.com>
2017-01-13 20:57:37 -0800

    quick-start added

--- Django/main/apps/first_app/views.py
+++ Django/main/apps/first_app/views.py
@@ -1,3 +1,15 @@
+# this is the controller (views)
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  print("*"*10)
+  print(request.method+" index method")
+  print("*"*10)
+  return render(request, "first_app/index.html")
+
+def show(request):
+  print("*"*10)
+  print(request.method+" show method")
+  print("*"*10)
+  return render(request, "first_app/show_users.html")
